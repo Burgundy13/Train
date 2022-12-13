d43301499c7ad9744a6f74335129f66f7de4b24f
Vlado Ivkovic <ivkovicv90@gmail.com>
2022-12-13 16:04:53 +0100

    train detail component

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './core/about/about.component';
+import { TicketFormComponent } from './ticket/ticket-form/ticket-form.component';
 import { TicketsListComponent } from './ticket/tickets-list/tickets-list.component';
 import { TrainDetailsComponent } from './train/train-details/train-details.component';
 import { TrainListComponent } from './train/train-list/train-list.component';
@@ -8,6 +9,7 @@ import { TrainListComponent } from './train/train-list/train-list.component';
 const routes: Routes = [
   { path: 'trains', component: TrainListComponent },
   { path: 'trains/:id', component: TrainDetailsComponent },
+  { path: 'trains/:id/ticketform', component: TicketFormComponent },
   { path: 'mytickets', component: TicketsListComponent },
   { path: 'about', component: AboutComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' },
